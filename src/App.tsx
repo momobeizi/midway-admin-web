49ab57f025f9cdb2a9a7407d083019261a182e3a
mrq <97624915@qq.com>
2024-10-31 11:15:42 +0800

    feat: 初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { HashRouter } from 'react-router-dom';
+import Router from './router';
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Router />
+    </HashRouter>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> pnpm i @types/node -D
+import UnoCSS from 'unocss/vite';
+
+function resolve(dir: string) {
+  return path.join(__dirname, '.', dir);
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [UnoCSS(), react()],
+  // 配置别名
+  resolve: {
+    alias: {
+      '@': resolve('src'), // 设置 `@` 指向 `src` 目录
+    },
+  },
+});
